aaf445282cebad69f77a1c0c6d95ebfd962a6100
knightofknee <bricarlis@gmail.com>
2017-12-07 20:54:29 -0600

    skeleton started

---
+++ src/components/homePage.vue
@@ -0,0 +1,53 @@
+<template>
+<div class='home-page'>
+  <div class='row' id='hero-image'>
+
+  </div>
+  <div class='row' id='who-why-header'>
+
+  </div>
+  <div class='row' id='who-why-text'>
+
+  </div>
+  <div class='row' id='read-more'>
+
+  </div>
+  <div class='row' id='action-carousel'>
+
+  </div>
+  <div class='row' id='testimonial-header'>
+
+  </div>
+  <div class='row' id='testimonial-carousel'>
+
+  </div>
+  <div id='partners'>
+    <div class='row' id='partners-header'>
+
+    </div>
+    <div class='row' id='partners-images'>
+
+    </div>
+    <div class='row' id='signup'>
+
+    </div>
+  </div>
+
+</div>
+
+</template>
+
+<script>
+  export default {
+    name: 'homePage',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
