a3bf1538f555c025c4da39168f504e419c4989cb
Alyssa Nickow <alyssa@nickow.com>
2017-12-11 19:04:17 -0600

    buttons for selecting categories, questions update based on button click

--- src/components/faq.vue
+++ src/components/faq.vue
@@ -1,8 +1,18 @@
-<!--An empty component to replace the header and/or footer on pages where it is not required-->
-
 <template>
   <div class="faq">
-    <h1>FAQ PAGE</h1>
+    <h1>Frequently Asked Questions</h1>
+    <p>Please select which category your question is regarding</p>
+    <div class="row">
+      <div id="category_buttons" class="col-sm-12 col-lg-6 col-lg-offset-3">
+        <button v-for="category in categories" v-on:click="curr_category = category">{{ category }}</button>
+      </div>
+    </div>
+    <div class="question row" v-for="faq in faqs" v-if="curr_category==faq.category || curr_category=='all'">
+      <div class="col-sm-12 col-lg-6 col-lg-offset-3">
+        <p>{{ faq.question }}</p>
+        <p>{{ faq.answer }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,10 +21,36 @@
     name: 'faq',
     data () {
       return {
+        categories: ["About Camp", "Parents", "Partners", "Volunteers", "Donations", "Media"],
+        curr_category: "all",
+        faqs: [
+          {question:"Sample Question About Camp", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
+          "labore et dolore magna aliqua.", category: "About Camp"},
+          {question:"Sample Question Parents", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
+          "labore et dolore magna aliqua.", category: "Parents"},
+          {question:"Sample Question Partners", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
+          "labore et dolore magna aliqua.", category: "Partners"},
+          {question:"Sample Question Volunteers", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
+          "labore et dolore magna aliqua.", category: "Volunteers"},
+          {question:"Sample Question Donations", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
+          "labore et dolore magna aliqua.", category: "Donations"},
+          {question:"Sample Question Media", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
+          "labore et dolore magna aliqua.", category: "Media"},
+        ]
       }
-    }
+    },
   }
 </script>
 
 <style scoped>
+  #category_buttons{
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 10px;
+  }
+
+  .question{
+    text-align: left;
+    margin: 10px;
+  }
 </style>
