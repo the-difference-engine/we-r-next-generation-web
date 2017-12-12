dbe28a23cfc4f903a50702d39950c6497b989b98
Alyssa Nickow <alyssa@nickow.com>
2017-12-11 19:39:18 -0600

    initial form for submitting questions - not connected to backend route for processing form

--- src/components/faq.vue
+++ src/components/faq.vue
@@ -13,7 +13,31 @@
         <p>{{ faq.answer }}</p>
       </div>
     </div>
+    <div class="row">
+      <div id="submit_question" class="col-sm-12 col-lg-6 col-lg-offset-3">
+        <h1>Have a Question?</h1>
+        <p>Hey, If there are any questions we did not answer, please feel free to contact us or leave us a feedback below.  Thanks for your support.</p>
+        <form class="form-horizontal" action="mailto:sample@email.go" method="post" enctype="text/plain">
+          <label for="name" class="col-sm-2 control-label">Name:</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="name">
+          </div><br><br>
+          <label for="email" class="col-sm-2 control-label">E-mail:</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="mail">
+          </div><br><br>
+          <label for="message" class="col-sm-2 control-label">Message:</label>
+          <div class="col-sm-10">
+            <textarea class="form-control" name="message" rows="3"></textarea>
+          </div><br><br>
+          <div class="col-sm-offset-2 col-sm-10">
+            <input id="submit" type="submit" value="Submit">
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -52,5 +76,15 @@
   .question{
     text-align: left;
     margin: 10px;
+  }
+
+  input, textarea, label{
+    margin: 10px auto;
+  }
+
+  #submit{
+    background-color:#EA8432;
+    width: 50%;
+    padding: 10px;
   }
 </style>
