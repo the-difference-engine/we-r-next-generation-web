b354537826dac3a908264bae96fb503580058fe1
knightofknee <bricarlis@gmail.com>
2017-12-11 20:59:14 -0600

    small improvements

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -1,10 +1,15 @@
 <template>
 <div class='home-page'>
-  <div class='row' id='hero-image'>
-    hero image with tagline on it
+  <div id='hero-image'>
+    <img src="https://upload.wikimedia.org/wikipedia/en/8/88/Mermaid_Man_and_Barnacle_Boy.jpg">
+    <h2 id="hero-words-positioning"><span id="hero-words">Text over image<br/>second line of text</span></h2>
   </div>
-  <div class='row' id='who-why-header'>
-    Who we are/ Why/ Cause
+  <div class='row'>
+    <h1>Who we are/ Why/ Cause</h1>
+    <p id="who-paragraph">this is a test text block this is a test text block this is a test text block this is a test text block this is a test text block this is a test text block hipatrick this is a test text block this is a test text block this is a test text block this is a test text block this is a test text block this is a test text block this is a test text block this is a test text block </p>
+  </div>
+  <div id="read-button" class='row'>
+    <button style="background-color:#FF4500" type="button" class="btn btn-primary btn-lg">Read more</button>
   </div>
   <div id='action-carousel'>
     <carousel navigationEnabled="true">
@@ -60,6 +65,34 @@
 </script>
 
 <style scoped>
+  #hero-image {
+    position: relative;
+  }
+  h2 {
+    position: absolute;
+    top: 100px;
+    left: 35%;
+  }
+  h2 span {
+    color: white;
+    font: bold 24px/45px Helvetica, Sans-Serif;
+    letter-spacing: -1px;
+    background: rgb(0, 0, 0); /* fallback color */
+    background: rgba(0, 0, 0, 0.7);
+    padding: 10px;
+  }
+  /* h2 span.spacer{
+    padding:0 5px;
+  } */
+  #who-paragraph {
+    margin: auto;
+    width: 80%;
+  }
+  #read-button {
+    text-align: right;
+    margin-right: 150px;
+    padding: 10px 0px;
+  }
   #action-carousel {
     width: 80%;
     margin: auto;
