53cf0cea0449afd1b3cb9721277c2d44ee5145b7
Alyssa Nickow <alyssa@nickow.com>
2017-12-14 19:36:48 -0600

    modified page to load all question and category content from api and database

--- src/components/faq.vue
+++ src/components/faq.vue
@@ -41,27 +41,29 @@
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
     name: 'faq',
     data () {
       return {
-        categories: ["About Camp", "Parents", "Partners", "Volunteers", "Donations", "Media"],
+        category_set: new Set(),
         curr_category: "all",
-        faqs: [
-          {question:"Sample Question About Camp", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
-          "labore et dolore magna aliqua.", category: "About Camp"},
-          {question:"Sample Question Parents", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
-          "labore et dolore magna aliqua.", category: "Parents"},
-          {question:"Sample Question Partners", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
-          "labore et dolore magna aliqua.", category: "Partners"},
-          {question:"Sample Question Volunteers", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
-          "labore et dolore magna aliqua.", category: "Volunteers"},
-          {question:"Sample Question Donations", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
-          "labore et dolore magna aliqua.", category: "Donations"},
-          {question:"Sample Question Media", answer:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut\n" +
-          "labore et dolore magna aliqua.", category: "Media"},
-        ]
+        faqs: []
       }
+    },
+    generate_categories() {
+
+    },
+    created() {
+      axios.get('http://localhost:4567/api/v1/faq').then(response => {
+        this.faqs = response.data
+        for (let faq of response.data) {
+          this.category_set.add(faq.category)
+        }
+        this.categories = Array.from(this.category_set)
+      }).catch(e => {
+        this.errors = e
+      })
     },
   }
 </script>
