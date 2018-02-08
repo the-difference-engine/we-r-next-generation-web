02a472b55ebc0ee4ed3442fed7b8646fa5d7a541
Alyssa Nickow <alyssa@nickow.com>
2018-02-07 20:07:41 -0600

    connected FAQ submission form to back end route for emailing

--- src/components/faq.vue
+++ src/components/faq.vue
@@ -14,27 +14,32 @@
         <p>{{ faq.answer }}</p>
       </div>
     </div>
-    <div class="row">
+    <div class="row" v-if="!questionSubmitted">
       <div id="submit_question" class="col-sm-12 col-lg-6 col-lg-offset-3">
         <h1>Have a Question?</h1>
         <p>Hey, If there are any questions we did not answer, please feel free to contact us or leave us a feedback below.  Thanks for your support.</p>
-        <form class="form-horizontal" action="mailto:sample@email.go" method="post" enctype="text/plain">
+        <form class="form-horizontal" v-on:submit.prevent="submitQuestion">
           <label for="name" class="col-sm-2 control-label">Name:</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" name="name">
+            <input v-model="newQuestion.name" class="form-control">
           </div><br><br>
           <label for="email" class="col-sm-2 control-label">E-mail:</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" name="mail">
+            <input v-model="newQuestion.email" class="form-control">
           </div><br><br>
           <label for="message" class="col-sm-2 control-label">Message:</label>
           <div class="col-sm-10">
-            <textarea class="form-control" name="message" rows="3"></textarea>
+            <textarea v-model="newQuestion.message" class="form-control" rows="3"></textarea>
           </div><br><br>
           <div class="col-sm-offset-2 col-sm-10">
             <input id="submit" type="submit" value="Submit">
           </div>
         </form>
+      </div>
+    </div>
+    <div class="row" v-if="questionSubmitted">
+      <div class="col-sm-12 col-lg-6 col-lg-offset-3">
+        <h1>Thank you for submitting your question.</h1>
       </div>
     </div>
   </div>
@@ -50,10 +55,24 @@
         category_set: new Set(),
         curr_category: "all",
         faqs: [],
+        newQuestion: {
+          name: "",
+          email: "",
+          message: "",
+        },
+        questionSubmitted: false
       }
     },
-    generate_categories() {
-
+    methods: {
+      submitQuestion: function(evt) {
+        axios.post('/api/v1/faq', this.newQuestion).then(response => {
+          this.questionSubmitted = true
+        }).catch( e=> {
+          this.errors = e
+        })
+//        Remove this line after the seahorse error has been corrected
+        this.questionSubmitted = true
+      }
     },
     created() {
       axios.get('/api/v1/faq').then(response => {
