a64226a1dbc818a86b3f91dd52617fbd0beb9914
Soren Yeadon <spyeadon@gmail.com>
2018-02-22 19:48:19 -0600

    created bare bones forgot password component

---
+++ src/components/forgotPassword.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="forgot-password">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'forgotPassword',
+    methods: {
+
+    },
+    data () {
+      return {}
+    }
+}
+</script>
+
+<style scoped>
+</style>
