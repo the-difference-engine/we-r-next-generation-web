63b61c6079c3a92098d71431b6deba2660c43542
knightofknee <bricarlis@gmail.com>
2018-03-01 19:27:49 -0600

    pushing just to share

--- src/components/wernxgHeader.vue
+++ src/components/wernxgHeader.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="header">
+  <div class="header container-fluid">
     <div id="header" class="row">
-      <div id="header_left" class="col-sm-3">
+      <div id="header-left" class="col-xs-3">
         <router-link to="/home">
         <img src="static/assets/WeRNextGeneration.png" alt="WeRNextGeneration Logo" width="200px">
         </router-link>
       </div>
-      <div id="header_right" class="col-sm-9">
-        <ul id="header_menu">
-          <li><router-link to="/campInfo">The Camp</router-link></li>
-          <li><router-link to="/successStories">Success Stories</router-link></li>
-          <li><router-link to="/volunteer">Get Involved</router-link></li>
-          <li v-if="(!this.loggedIn && !loginStatus)"><router-link to="/login">Log In</router-link></li>
-          <li v-if="(this.loggedIn || loginStatus)"><button
-            class="btn btn-primary"
-            v-on:click.prevent="submitLogout">Logout
-          </button></li>
-          <li v-if="(!this.loggedIn && !loginStatus)"><router-link to="/signup">Sign Up</router-link></li>
-          <li ><router-link to="/donate"> <button id="donate" class="btn btn-primary btn-md">Donate</button></router-link></li>
-          <li><router-link to="/profile" v-html="profileImage"></router-link></li>
-        </ul>
-        <div id="menu-button" class="col-sm-9"> menu
-          <img src="static/assets/hamburger-menu-icon.svg" alt="Menu Button"/>
+      <div id="header-right" class="col-xs-9">
+        <div id="right-container">
+          <ul id="header_menu">
+            <li><router-link to="/campInfo">The Camp</router-link></li>
+            <li><router-link to="/successStories">Success Stories</router-link></li>
+            <li><router-link to="/volunteer">Get Involved</router-link></li>
+            <li v-if="(!this.loggedIn && !loginStatus)"><router-link to="/login">Log In</router-link></li>
+            <li v-if="(this.loggedIn || loginStatus)"><button
+              class="btn btn-primary"
+              v-on:click.prevent="submitLogout">Logout
+            </button></li>
+            <li v-if="(!this.loggedIn && !loginStatus)"><router-link to="/signup">Sign Up</router-link></li>
+            <li ><router-link to="/donate"> <button id="donate" class="btn btn-primary btn-md">Donate</button></router-link></li>
+            <li><router-link to="/profile" v-html="profileImage"></router-link></li>
+          </ul>
+          <div id="menu-button">
+            <img src="static/assets/hamburger-menu-icon.svg" alt="Menu Button"/>
+          </div>
         </div>
       </div>
     </div>
@@ -80,27 +82,63 @@
     color: black;
   }
   #header {
+    margin-bottom: 5px;
   }
-
+  #header-left {
+    padding-left: 30px;
+  }
+  #header-right {
+    /* padding-left: 30px; */
+    display: flex;
+    justify-content: flex-end;
+  }
+  /* header right seems to wrap just under 945 width, need to change sizes or something */
+/*//////////////////// */
   /*Header Styling*/
   #header_menu{
     list-style: none;
   }
-
   #header_menu>li{
     display: inline-block;
-    margin: 10px 15px;
+    padding: 10px 2px;
   }
   #menu-button {
     display: none;
   }
-
-  @media (max-width: 768px) {
+  @media (max-width: 700px) {
     #header_menu {
       display: none;
       }
     #menu-button {
       display: inline-block
+    }
+    #right-container {
+      padding-right: 20%;
+      padding-top: 5px;
+    }
+  }
+  @media (min-width: 755px) {
+    #header_menu>li{
+    display: inline-block;
+    padding: 10px 5px;
+    }
+  }
+    @media (min-width: 850px) {
+    #header_menu>li{
+    display: inline-block;
+    padding: 10px 10px;
+    }
+    #right-container {
+      padding-right: 1.8%;
+    }
+  }
+  @media (min-width: 1040px) {
+    #header_menu>li{
+    display: inline-block;
+    padding: 10px 20px;
+    }
+    #right-container {
+      padding-right: 3.5%;
     }
   }
 
