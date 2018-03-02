b1c734443b6da9183558ecb40e00d27c41fa6715
Alyssa Nickow <alyssa@nickow.com>
2018-03-01 19:13:41 -0600

    adjusted styling for opporunities/get involved page to reflect responsive designs

--- src/components/opportunities.vue
+++ src/components/opportunities.vue
@@ -2,22 +2,21 @@
   <div class="opportunities">
     <h1>Opportunities</h1>
     <h3>...to foster creativity in our kids</h3>
-    <div v-for="entry in pageContent">
+    <div v-for="entry in pageContent" class="opportunity">
       <div class="row">
-        <div class="col-lg-5 col-lg-offset-1"><img :src="entry.imgSrc" :alt="entry.imgAlt" width="80%"></div>
-        <div class="col-lg-4 caption">
+        <div class="col-sm-5 col-xs-offset-1 col-xs-10"><img :src="entry.imgSrc" :alt="entry.imgAlt" class="img-responsive"></div>
+        <div class="col-sm-5 col-sm-offset-0 col-xs-10 col-xs-offset-1 caption">
           <h3>{{ entry.headline }}</h3>
           <p>{{ entry.caption }}</p>
           <p>{{ entry.callToAct }}</p>
         </div>
       </div>
-      <br>
     </div>
     <div class="row">
-      <div class="col-lg-6 col-lg-offset-3">
+      <div class="col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
         <h3>If you are interested in helping children embrace their creative side, sign up and submit an application</h3>
         <router-link to="/signup">
-          <button id="getInvolved" type="button" class="btn btn-primary btn-lg">Get Involved</button>
+          <button id="getInvolved" type="button" class="btn btn-primary btn-lg">Sign Up</button>
         </router-link>
       </div>
     </div>
@@ -51,6 +50,11 @@
 
   h2{
     font-weight: bolder
+  }
+
+  .opportunity{
+    margin-bottom: 50px;
+    margin-top: 45px;
   }
 
   .caption{
