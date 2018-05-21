93b03d41ea808ae55f64a25f475ae86d2bc06115
Allyson <allyson.frazier09@gmail.com>
2018-05-21 10:15:59 -0500

    started search filter

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,52 +1,52 @@
 <template>
-  <div class="users container">
-    <h1>Users</h1>
-    <div class="form-group">
-      <label for="user-filter">Filter by:</label>
-      <select id="user-filter" class="form-control" v-model="filterRole">
-        <option value="">Filter by&hellip;</option>
-        <option
-          :key="role"
-          :value="role"
-          v-for="role in roles"
-        >{{ role }}</option>
-      </select>
+  <div class="users container-fluid">
+    <div id="title" class="row">
+      <h1 class="col-md-6">Users</h1>
+      <i id="userIcon" class="fa fa-users col-md-6"></i>
     </div>
-    <table class="table table-bordered table-striped">
-      <thead>
-        <tr>
-          <sortable-th
-            :column="column"
-            :key="column"
-            :sort-ascending="sortAscending"
-            :sort-column="sortColumn"
-            @sort="sortBy"
-            class="sortable-th"
-            v-for="column in sortColumns"
-          ></sortable-th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in sortedUsers" :key="user.id">
-          <th>{{ user.id }}</th>
-          <td>{{ user.name }}</td>
-          <td>{{ user.email }}</td>
-          <td>
-            <span
-              v-for="application in user.applications"
-              :key="application.id"
-            >
-              {{ application.type }}
-            </span>
-          </td>
-          <td>{{ getMostRecentApplicationDate(user) }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <input type="text" v-model="filterTest" placeholder="search...">
+      <p>Here is: {{ filterTest }}</p>
+      <!-- {{ users.map(u => {
+        console.log('MAP STUFF');
+        console.log(u);
+        console.log(u.name);
+      }) }} -->
+      <!-- {{all.map(function(user){if (user.contains(filterTest))return <p>entry.name</P})}} -->
+    </div>
+    <div id="blackLine">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>
+              <a>Name</a>
+            </th>
+            <th>
+              <a>Email</a>
+            </th>
+            <th>
+              <a>Registered</a>
+            </th>
+            <th>
+              <a>Role</a>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in users" v-bind:key="index">
+            <td>{{ user.full_name }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user._id.$oid }}</td>
+            <td>{{ user.role }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+  import localforage from '../sessionUtils'
   import axios from 'axios';
   import sortableTh from '@/components/sortableTh';
 
@@ -55,64 +55,32 @@
   const roles = ['admin', 'parent', 'sponsor', 'volunteer'];
 
   export default {
-    name: 'users',
-    components: {
-      'sortable-th': sortableTh,
-    },
-    data: function() {
+    data(){
       return {
-        filterRole: "",
-        roles: roles,
-        sortAscending: defaultSortAscending,
-        sortColumn: "id",
-        sortColumns: sortColumns,
+        filterTest: '',
         users: [],
+        result: [],
+        columns: ['name', 'email', 'registered', 'role'],
+        status: {
+          name: 'false',
+          email: 'false',
+          registered: 'false',
+          role: 'false'
+        }
       }
     },
     computed: {
-      sortedUsers: function() {
-        return this.filteredUsers.sort((left, right) => {
-          let leftColumn = null;
-          let rightColumn = null;
-          switch (this.sortColumn) {
-            case 'date':
-              leftColumn = this.getMostRecentApplicationDate(left);
-              rightColumn = this.getMostRecentApplicationDate(right);
-              break;
-            case 'roles':
-              leftColumn = left.applications.length;
-              rightColumn = right.applications.length;
-              break;
-            default:
-              leftColumn = left[this.sortColumn];
-              rightColumn = right[this.sortColumn];
-          }
-
-          let sort = 0;
-          if (leftColumn > rightColumn) {
-            sort = 1;
-          } else if(leftColumn < rightColumn) {
-            sort = -1;
-          }
-
-          return (this.sortAscending ? sort : -sort);
-        });
-      },
-      filteredUsers: function() {
-        return this.users.filter(user => {
-          let userRoles = user.applications.map(application => { return application.type; });
-          return this.filterRole === "" || userRoles.includes(this.filterRole);
-        });
-      },
     },
     methods: {
       sortBy: function(currentColumn) {
-        if (this.sortColumn === currentColumn) {
-          this.sortAscending = !this.sortAscending;
-        } else {
-          this.sortAscending = defaultSortAscending;
-        }
-        this.sortColumn = currentColumn;
+        console.log("IN SORT BY METHOD");
+        console.log(currentColumn);
+        // if (this.sortColumn === currentColumn) {
+        //   this.sortAscending = !this.sortAscending;
+        // } else {
+        //   this.sortAscending = defaultSortAscending;
+        // }
+        // this.sortColumn = currentColumn;
       },
       getMostRecentApplicationDate: function(user) {
         let dates = [];
@@ -123,13 +91,25 @@
 
         return sortedDates.pop();
       },
+      // filterUsers: function() {
+      //   this.users.map(u => {
+      //     console.log("CONSOLES-------------");
+      //     console.log(u.name);
+      //     console.log(u);
+      //   })
+      // }
     },
     created() {
-      axios.get('/api/v1/users').then(response => {
-        this.users = response.data;
-      }).catch(errors => {
-        this.errors = errors;
+      localforage.getItem('X_TOKEN')
+      .then(session => {
+        axios.get('/api/v1/profiles', { 'headers': { 'x-token': session } })
+        .then(response => {
+          this.users = response.data
+          console.log("USERS FOUND", this.users)
+        })
+        .catch(console.error)
       })
+      .catch(console.error)
     }
   }
 </script>
@@ -137,5 +117,35 @@
 <style scoped>
   .sortable-th {
     cursor: pointer;
+  }
+  #title {
+    margin: auto;
+    max-width: 300px;
+    display: inline-block;
+    vertical-align: bottom;
+  }
+  #userIcon {
+    font-size: 35px;
+    margin-top: 20px;
+    color: #337ab7;
+  }
+  #blackLine {
+    border-top: 1px solid black;
+  }
+  #viewOptions {
+    margin-top: 10px;
+  }
+  body {
+  margin: 2em 0;
+  }
+
+  a {
+    font-weight: bold;
+    color: #337ab7;
+  }
+
+  a.active {
+    font-weight: bold;
+    color: black;
   }
 </style>
