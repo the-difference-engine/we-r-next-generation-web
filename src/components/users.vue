a7230b1ae1b10fa1f4a45f403b6620056105c3a2
Allyson <allyson.frazier09@gmail.com>
2018-05-22 09:38:12 -0500

    finished search and table

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,40 +1,48 @@
 <template>
-  <div class="users container-fluid">
-    <div id="title" class="row">
-      <h1 class="col-md-6">Users</h1>
-      <i id="userIcon" class="fa fa-users col-md-6"></i>
-    </div>
-    <div>
-      <input type="text" v-model="filterTest" v-on:change="testing" placeholder="search...">
-      <p>{{ filteredUsers }}</p>
-    </div>
-    <div id="blackLine">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>
-              <a>Name</a>
-            </th>
-            <th>
-              <a>Email</a>
-            </th>
-            <th>
-              <a>Registered</a>
-            </th>
-            <th>
-              <a>Role</a>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(user, index) in users" v-bind:key="index">
-            <td v-if="user">{{ user.full_name }}</td>
-            <td v-if="user">{{ user.email }}</td>
-            <td v-if="user">{{ user._id.$oid }}</td>
-            <td v-if="user">{{ user.role }}</td>
-          </tr>
-        </tbody>
-      </table>
+  <div id="wrapper">
+    <div class="users container">
+      <div id="title" class="row">
+        <h1 class="col-md-6">Users</h1>
+        <i id="userIcon" class="fa fa-users col-md-6"></i>
+      </div>
+      <div id="testContainer">
+        <i id="searchIcon" class="fa fa-search"></i>
+        <input id="searchbar" type="text" v-model="filterString" placeholder="search..." class="form-control form-group">
+        <p>{{ filterUsers }}</p>
+        <!-- <p>{{ testing }}</p> -->
+      </div>
+      <div id="blackLine">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>
+                <a v-on:click="sortUsers('Name')">Name</a>
+                <i class="arrows" v-bind:class="status.Name"></i>
+              </th>
+              <th>
+                <a  v-on:click="sortUsers('Email')">Email</a>
+                <i class="arrows" v-bind:class="status.Email"></i>
+              </th>
+              <th>
+                <a  v-on:click="sortUsers('Registered')">Registered</a>
+                <i class="arrows" v-bind:class="status.Registered"></i>
+              </th>
+              <th>
+                <a  v-on:click="sortUsers('Role')">Role</a>
+                <i class="arrows" v-bind:class="status.Role"></i>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(user, index) in filteredUsers" v-bind:key="index">
+              <td v-if="user">{{ user.full_name }}</td>
+              <td v-if="user">{{ user.email }}</td>
+              <td v-if="user">{{ user._id.$oid }}</td>
+              <td v-if="user">{{ user.role }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -43,6 +51,7 @@
   import localforage from '../sessionUtils'
   import axios from 'axios';
   import sortableTh from '@/components/sortableTh';
+  import _ from 'lodash';
 
   const defaultSortAscending = true;
   const sortColumns = ['id', 'name', 'email', 'roles', 'date']
@@ -51,56 +60,63 @@
   export default {
     data(){
       return {
-        filterTest: '',
+        filterString: '',
         filteredUsers: [],
         users: [],
-        result: [],
         columns: ['name', 'email', 'registered', 'role'],
         status: {
-          name: 'false',
-          email: 'false',
-          registered: 'false',
-          role: 'false'
+          Name: 'fa fa-angle-right',
+          Email: 'fa fa-angle-right',
+          Registered: 'fa fa-angle-down',
+          Role: 'fa fa-angle-right'
         }
       }
     },
     computed: {
-      // filterUsers: function() {
-      //   console.log('GOT TO FILTERUSERS');
-      //   this.filteredUsers = this.users.map(u => {
-      //     if (u.full_name.includes(this.filterTest)) {
-      //       console.log('MATCHES');
-      //       console.log(u.full_name)
-      //       return u;
-      //     }
-      //   });
-      // }
-    },
-    methods: {
-      sortBy: function(currentColumn) {
-        console.log("IN SORT BY METHOD");
-        console.log(currentColumn);
-        // if (this.sortColumn === currentColumn) {
-        //   this.sortAscending = !this.sortAscending;
-        // } else {
-        //   this.sortAscending = defaultSortAscending;
-        // }
-        // this.sortColumn = currentColumn;
-      },
-      testing: function() {
-        console.log('GOT TO FILTERUSERS');
+      filterUsers: function() {
         this.filteredUsers = this.users.map(u => {
-          if (u.full_name.includes(this.filterTest)) {
-            console.log('MATCHES');
-            console.log(u.full_name)
+          let date =  new Date(parseInt(u._id.$oid.substring(0, 8), 16) * 1000);
+          let shortDate = '' + date
+          u._id.$oid = shortDate.slice(4,15)
+          if (u.full_name.toLowerCase().includes(this.filterString.toLowerCase()) || u.email.toLowerCase().includes(this.filterString.toLowerCase())) {
             return u;
           }
-        this.users = this.filteredUsers;
-        console.log("NEW USERS LSIT");
-        console.log(this.users);
-        console.log("FILTER LIST");
-        console.log(this.filteredUsers);
         });
+      },
+      testing: function() {
+        return dateFromObjectId =  new Date(parseInt(oid.substring(0, 8), 16) * 1000);
+      }
+    },
+    methods: {
+      sortUsers: function(column) {
+        if (column === 'Name') {
+          this.status.Name = 'fa fa-angle-down';
+          this.status.Email = 'fa fa-angle-right';
+          this.status.Registered = 'fa fa-angle-right';
+          this.status.Role = 'fa fa-angle-right';
+          this.filteredUsers = _.orderBy(this.filteredUsers, 'full_name');
+        }
+        if (column === 'Email') {
+          this.status.Name = 'fa fa-angle-right';
+          this.status.Email = 'fa fa-angle-down';
+          this.status.Registered = 'fa fa-angle-right';
+          this.status.Role = 'fa fa-angle-right';
+          this.filteredUsers = _.orderBy(this.filteredUsers, 'email');
+        }
+        if (column === 'Registered') {
+          this.status.Name = 'fa fa-angle-right';
+          this.status.Email = 'fa fa-angle-right';
+          this.status.Registered = 'fa fa-angle-down';
+          this.status.Role = 'fa fa-angle-down';
+          this.filteredUsers = this.users;
+        }
+        if (column === 'Role') {
+          this.status.Name = 'fa fa-angle-right';
+          this.status.Email = 'fa fa-angle-right';
+          this.status.Registered = 'fa fa-angle-right';
+          this.status.Role = 'fa fa-angle-down';
+          this.filteredUsers = _.orderBy(this.filteredUsers, 'role');
+        }
       },
       getMostRecentApplicationDate: function(user) {
         let dates = [];
@@ -117,8 +133,9 @@
       .then(session => {
         axios.get('/api/v1/profiles', { 'headers': { 'x-token': session } })
         .then(response => {
-          this.users = response.data
-          console.log("USERS FOUND", this.users)
+          this.users = response.data.reverse()
+          this.filteredUsers = response.data.reverse()
+          
         })
         .catch(console.error)
       })
@@ -142,6 +159,10 @@
     margin-top: 20px;
     color: #337ab7;
   }
+  i {
+    color: #337ab7;
+    font-weight: bold;
+  }
   #blackLine {
     border-top: 1px solid black;
   }
@@ -161,4 +182,35 @@
     font-weight: bold;
     color: black;
   }
+
+  #searchbar {
+    margin-top: 5px;
+    width: 25%;
+    display: inline-block;
+  }
+
+  #searchIcon {
+    font-size: 25px;
+    margin-right: 8px;
+  }
+
+  #testContainer{
+    justify-content: center;
+  }
+
+  th {
+    text-align-last: center;
+  }
+
+  #wrapper {
+    display: flex;
+    justify-content: center;
+  }
+
+  .arrows {
+    position: absolute;
+    padding-left: 5px;
+    padding-top: 2px;
+  }
+
 </style>
